b83694d44d60c3fa411e8c002b490760eafb1c4d
solomon <adamtblack96@gmail.com>
2018-02-26 16:27:06 -0800

    My flask app

---
+++ MyProjects/Py/MyFlaskApp/data.py
@@ -0,0 +1,35 @@
+def Articles():
+	articles = [
+
+{
+	  'id': 1,
+	  'title':'Article One',
+	  'body':'bjhsdbvjdbvkjslvsdvbbvjb',
+	  'author':'Solomon Taremwa',
+	  'create_date':'15-02-2018' 
+
+
+	},
+
+	{
+	  'id': 2,
+	  'title':'Article Two',
+	  'body':'bjhsdbvjdbvkjslvsdvbbvjb',
+	  'author':'Jane Doe',
+	  'create_date':'15-02-2018' 
+
+
+	},
+
+{
+	  'id': 3,
+	  'title':'Article Three',
+	  'body':'bjhsdbvjdbvkjslvsdvbbvjb',
+	  'author':'Sam Sam',
+	  'create_date':'15-02-2018' 
+
+
+	},
+
+	]
+	return articles
